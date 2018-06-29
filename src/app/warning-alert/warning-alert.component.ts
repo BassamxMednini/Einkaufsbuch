c976299bb4ccbc3aef89b4c39e565164f0d524e8
Bassam <Bassam.Mednini.1997@gmail.com>
2018-06-29 17:34:37 +0200

    Testzwecke

--- src/app/warning-alert/warning-alert.component.ts
+++ src/app/warning-alert/warning-alert.component.ts
@@ -3,11 +3,34 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-warning-alert',
   templateUrl: './warning-alert.component.html',
-  styleUrls: ['./warning-alert.component.css']
+  styleUrls: ['./warning-alert.component.css'],
+  styles: [`
+        .online {
+            color: white;
+        }
+    `]
 })
 export class WarningAlertComponent implements OnInit {
+  showPassword = false;
+  showTime = [];
 
   constructor() { }
+
+  showElement() {
+    if (this.showPassword === false) {
+      this.showPassword = true;
+      this.showTime.push(Date.now());
+    } else {
+      this.showPassword = false;
+      this.showTime.push(Date.now());
+    }
+  }
+
+  getColor() {
+    if (this.showTime.length > 5) {
+      return 'red';
+    }
+  }
 
   ngOnInit() {
   }
