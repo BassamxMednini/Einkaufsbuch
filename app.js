1f94b01c68727b6987c677346d19ef1d7855e19a
Bassam Mednini <Bassam.Mednini.1997@gmail.com>
2018-09-23 18:03:30 +0200

    pug installiert und 2 Routes erstellt

--- app.js
+++ app.js
@@ -1,7 +1,39 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var express = require('express');
+var app = express();
+app.set('view engine', 'pug');
 
-app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/', function (req, res) {
+    let articles = [
+        {
+            id: 1,
+            title: 'Artikel 1',
+            author: 'Bassam Mednini',
+            body: 'Das ist Artikel 1'
+        },
+        {
+            id: 2,
+            title: 'Artikel 2',
+            author: 'Bassam Mednini',
+            body: 'Das ist Artikel 2'
+        },
+        {
+            id: 3,
+            title: 'Artikel 3',
+            author: 'Bassam Mednini',
+            body: 'Das ist Artikel 3'
+        }
+    ]
+    res.render('index', {
+        title: 'Mednini',
+        articles: articles
+    })
+})
+
+app.get('/articles/add', function(req, res) {
+    res.render('add_article', {
+        title: 'Add Article'
+    })
+})
+
+var server = app.listen(3000, function () {});
