b845ecf57b86d8c2ca592c9fed2f242431da162d
Bassam Mednini <Bassam.Mednini.1997@gmail.com>
2018-10-04 08:25:34 +0200

    Für bessere Orientierung Kommentare hinzugefügt

--- app.js
+++ app.js
@@ -4,27 +4,34 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+// Verbindung zur Datenbank
 mongoose.connect('mongodb://localhost/nodekb')
 let db = mongoose.connection;
 
+// Bei erfolgreicher Verbindung wird diese dementsprechend angezeigt
 db.once('open', function() {
     console.log('Connected to MongoDB');
 });
 
+// Bei nicht erfolgreicher Verbindung wird diese dementsprechend angezeigt
 db.on('error', function(err) {
     console.log(err);
 });
 
+// Dient für die DB - Schema
 let Article = require('./models/article');
 
-// parse application/x-www-form-urlencoded
+
 app.use(bodyParser.urlencoded({ extended: false }))
-// parse application/json
 app.use(bodyParser.json())
 
-
+// Ordner 'public' wird als statische Datei bereitgestellt
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Folgende Dateien werden ebenfalls als statische Datei gestellt
+// Beim Arbeiten mit HTML (unter .pug) reicht es die Datei mit Hilfe
+// der Angabe des Pfads sowie den Name der Datei einzufügen
+// z.B: für '/bootstrap' --> '/bootstrap/bootstrap.css'
 app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.use('/styles', express.static(__dirname + '/public/css'));
 app.use('/bootstrap/javascript', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
@@ -32,22 +39,26 @@ app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
 app.use('/popper', express.static(__dirname + '/node_modules/popper.js/dist'));
 app.use('/js', express.static(__dirname + '/public/js'));
 
+// Template-Engine Pug wird in dieser Applikation verwendet
 app.set('view engine', 'pug');
 
+// Startseite
 app.get('/', function (req, res) {
     Article.find({}, function(err, articles){
         if(err){
             console.log(err);
         } else {
             res.render('index', {
-                title: 'Mednini',
+                title: 'NodeJS Applikation',
                 articles: articles
             }); 
         }
     });
 });
 
-
+// Öffnet jeden DB-Eintrag über /article/:id
+// Neben dem Titel und der Beschreibung, wird auch der Autor angezeigt
+// Lösch- & Bearbeitenbutton werden ebenfalls dann angezeigt
 app.get('/article/:id', function(req, res) {
     Article.findById(req.params.id, function(err, article){
         res.render('article', {
@@ -56,27 +67,32 @@ app.get('/article/:id', function(req, res) {
     });
 });
 
+// Bearbeiten-Button
 app.get('/article/edit/:id', function(req, res) {
     Article.findById(req.params.id, function(err, article){
         res.render('edit_article', {
-            title: 'Edit Article',
+            title: 'Artikel ändern',
             article: article
         });
     });
 });
 
+// Hinzufügen eines neuen DB-Eintrags
 app.get('/articles/add', function(req, res) {
     res.render('add_article', {
-        title: 'Add Article'
+        title: 'Hinzufügen eines Artikels'
     })
 })
 
+// Speichert den neuen Artikel in die DB
 app.post('/articles/add', function(req, res) {
     let article = new Article();
     article.title = req.body.title;
     article.author = req.body.author;
     article.body = req.body.body;
 
+    // Sofern beim Erstellen ein Fehler existiert, wird dieser über die Konsole angezeigt
+    // Ansonsten wird man zur Startseite weitergeleitet
     article.save(function(err) {
         if (err) {
             console.log(err);
@@ -87,7 +103,7 @@ app.post('/articles/add', function(req, res) {
     })
 })
 
-// Update 
+// Aktualisierter Artikel wird in der DB gespeichert
 app.post('/articles/edit/:id', function(req, res) {
     let article = {};
     article.title = req.body.title;
@@ -118,5 +134,5 @@ app.delete('/article/:id', function(req, res) {
     });
 });
 
-
+// Server
 var server = app.listen(3000, function () {});
