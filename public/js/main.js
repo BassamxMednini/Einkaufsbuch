19c2b5afe7c7d35f3ba92853efbf287d63cc52af
Bassam Mednini <Bassam.Mednini.1997@gmail.com>
2018-10-04 08:26:10 +0200

    Für bessere Orientierung Kommentare hinzugefügt

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,5 @@
+// Um Artikel einer DB zu löschen, muss eine Ajax-Request gemacht werden
+// Mit Hilfe von jQuery wird die Request durchgeführt
 $(document).ready(function() {
     $('.delete-article').on('click', function(e){
         $target = $(e.target);
@@ -6,7 +8,7 @@ $(document).ready(function() {
             type: 'DELETE',
             url: '/article/' + id,
             success: function(response) {
-                alert('Deleting Article');
+                alert('Artikel gelöscht');
                 window.location.href='/';
             },
             error: function(err) {
